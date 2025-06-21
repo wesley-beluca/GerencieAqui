98bfd3463e746bcc37fd65291ac46ff1e0713ddc
Wesley Beluca <wesleybeluca@gmail.com>
2025-06-21 02:01:18 -0300

    aumenta timeout

--- src/store/api.js
+++ src/store/api.js
@@ -5,13 +5,12 @@ import router from '../router'
 // Configuração do Axios
 const api = axios.create({
   baseURL: 'https://localhost:7045/api',
-  timeout: 10000,
+  timeout: 30000, 
   headers: {
     'Content-Type': 'application/json'
   }
 })
 
-// Interceptor para adicionar token em todas as requisições
 api.interceptors.request.use(
   config => {
     const token = localStorage.getItem('token')
